64d0450782135b35d161bd33665d32e6393eb27d
Mikhail Garbuzov <madmxg@gmail.com>
2016-11-23 23:56:39 +0300

    download module elements and cache it

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ class BetBackground {
     app.ttl = app.ttl || 1000 * 60 * 1;
 
     this.ttl = app.ttl;
-    this.cache = LRU({
-      maxAge: app.ttl
-    });
+    this.storage = {
+      config: localforage.createInstance({ name: 'config' }),
+      modules: localforage.createInstance({ name: 'modules' })
+    };
+    this.cache = {
+      config: LRU({ maxAge: app.ttl}),
+      modules: LRU({ maxAge: 1000 * 60 * 60 * 24 })
+    };
   }
 
   run () {
@@ -38,27 +43,30 @@ class BetBackground {
   getConfig () {
     return Promise.resolve()
       .then(() => {
-        if (this.cache.has('config')) {
+        if (this.cache.config.has('config')) {
           console.log('from cache');
-          return Promise.resolve(this.cache.get('config'));
+          return Promise.resolve(this.cache.config.get('config'));
         }
 
         return Promise.resolve()
-          .then(() => localforage.getItem('config'))
+          .then(() => this.storage.config.getItem('config'))
           .then(config => {
+            // config not found in storage
             if (!config) {
               return this.loadConfig();
             }
             let ttl = config.__ttl - new Date().valueOf();
 
-            this.cache.set('config', config, ttl);
+            this.cache.config.set('config', config, ttl);
 
-            if (this.cache.has('config')) {
+            // config found and fresh
+            if (this.cache.config.has('config')) {
               console.log('from ls', ttl);
-              return Promise.resolve(this.cache.get('config'));
+              return Promise.resolve(this.cache.config.get('config'));
             }
 
-            return this.loadConfig();
+            // config found, but isn`t fresh, load new from server
+            return this.loadConfig(config);
           });
       })
       .then(config => {
@@ -67,7 +75,7 @@ class BetBackground {
       });
   }
 
-  loadConfig () {
+  loadConfig (oldConfig) {
     return Promise.resolve()
       .then(() => fetch('http://localhost:3000/config.json'))
       .then(response => {
@@ -77,18 +85,114 @@ class BetBackground {
         return response.json();
       })
       .then(config => {
-        config.__ttl = new Date().valueOf() + this.ttl;
-        this.cache.set('config', config, this.ttl);
-        console.log('from server');
+        if (
+          oldConfig
+          &&
+          (
+            oldConfig[0] && config[0]
+          )
+          &&
+          (
+            'number' === typeof oldConfig[0].v
+            &&
+            'number' === typeof config[0].v
+          )
+          &&
+          (
+            oldConfig[0].v !== config[0].v
+          )
+        ) {
+          return this.wipeModules(config);
+        }
 
+        return Promise.resolve(config);
+      })
+      .then(config => {
         return Promise.resolve()
-          .then(() => localforage.setItem('config', config))
+          .then(() => {
+            config.__ttl = new Date().valueOf() + this.ttl;
+            this.cache.config.set('config', config, this.ttl);
+            console.log('from server');
+            return Promise.resolve();
+          })
+          .then(() => this.storage.config.setItem('config', config))
           .then(() => Promise.resolve(config));
       });
   }
 
-  getModules () {
+  wipeModules (config) {
+    console.log('wipe!');
+    return Promise.resolve()
+      .then(() => this.cache.modules.reset())
+      .then(() => this.storage.modules.clear())
+      .then(() => Promise.resolve(config));
+  }
+
+  getModules (config) {
+    return Promise.resolve()
+      .then(() => {
+        // remove system config element
+        config.shift();
+        const parallel = config.map(moduleConfig => this.getModule(moduleConfig));
+        return Promise.all(parallel);
+      });
+  }
+
+  getModule (moduleConfig) {
+    if (Array.isArray(moduleConfig.l) && moduleConfig.l.length) {
+      return this.getModuleElements(moduleConfig.l);
+    }
+
     return Promise.resolve();
+  }
+
+  getModuleElements (links) {
+    const parallel = links.map(link => this.getModuleElement(link));
+    return Promise.all(parallel);
+  }
+
+  getModuleElement (link) {
+    if (this.cache.modules.has(link)) {
+      console.log(`from cache: ${link}`);
+      return Promise.resolve();
+    }
+
+    return Promise.resolve()
+      .then(() => this.storage.modules.getItem(link))
+      .then(text => {
+        // link text not found in storage
+        if (!text) {
+          return this.loadModuleElement(link);
+        }
+
+        this.cache.modules.set(link, text);
+
+        // link text found and fresh
+        if (this.cache.modules.has(link)) {
+          console.log(`from ls: ${link}`);
+          return Promise.resolve();
+        }
+
+        // link text found, but isn`t fresh, load new from server
+        return this.loadModuleElement(link);
+      });
+  }
+
+  loadModuleElement (link) {
+    return Promise.resolve()
+      .then(() => fetch(link))
+      .then(response => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.text();
+      })
+      .then(text => {
+        console.log(`from server: ${link}`);
+        return Promise.resolve()
+          .then(() => this.cache.modules.set(link, text))
+          .then(() => this.storage.modules.setItem(link, text));
+      });
   }
 };
 
