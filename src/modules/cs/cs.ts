8b070741e8cb7c408f609e517b57282b59ec44a5
Mikhail Garbuzov <madmxg@gmail.com>
2022-03-10 23:14:48 +0300

    add ContentScript

--- src/modules/cs/cs.ts
+++ src/modules/cs/cs.ts
@@ -1,14 +1,26 @@
+import 'reflect-metadata';
+
+import { Container, Service } from 'typedi';
 import { MessageReasonEnum } from '../constants';
 import { ControllerMessage } from '../message-controller/interfaces';
-
-console.log('cs');
 
 const message: ControllerMessage = {
   reason: MessageReasonEnum.GET_SOURCES,
 };
-chrome.runtime.sendMessage(null, message, null, (data) => {
-  const scriptTag = document.createElement('script');
-  scriptTag.type = 'text/javascript';
-  scriptTag.text = data[0];
-  document.body.appendChild(scriptTag);
-});
+
+@Service()
+class ContentScript {
+  public async init(): Promise<void> {
+    chrome.runtime.sendMessage(null, message, null, (data) => {
+      const scriptTag = document.createElement('script');
+      scriptTag.type = 'text/javascript';
+      scriptTag.text = data[0];
+      document.body.appendChild(scriptTag);
+    });
+  }
+}
+
+const contentScript = Container.get(ContentScript);
+contentScript.init();
+
+console.log('cs');
